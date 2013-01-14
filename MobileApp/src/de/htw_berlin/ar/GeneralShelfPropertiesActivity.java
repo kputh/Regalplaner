8adf5f53346efdf7f15ecfc557030f3021ab8f08
s0541303 <s0541303@756f71c7-1ed1-4163-b6ac-367b62af1077>
2013-01-14 23:06:35 +0000

    Wireframes 1, 3, 4a and 4b realized.
    -Kai

--- MobileApp/src/de/htw_berlin/ar/GeneralShelfPropertiesActivity.java
+++ MobileApp/src/de/htw_berlin/ar/GeneralShelfPropertiesActivity.java
@@ -5,10 +5,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class GeneralShelfPropertiesActivity extends Activity {
+public class GeneralShelfPropertiesActivity extends Activity implements OnItemSelectedListener {
+	
+	private enum Shape { ROUND, SQUARE, OVAL, RECTANGULAR, NULL }
+	private Shape shape;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,10 @@ public class GeneralShelfPropertiesActivity extends Activity {
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
 		spinner.setAdapter(adapter);
+		
+		spinner.setOnItemSelectedListener(this);
+		
+		shape = Shape.NULL;
 	}
 
 	@Override
@@ -35,7 +44,58 @@ public class GeneralShelfPropertiesActivity extends Activity {
 	
 	public void nextScreen(View view) {
 		
-    	Intent intent = new Intent(this, GeneralShelfPropertiesActivity.class);
-    	startActivity(intent);
+    	Intent intent;
+    	switch(shape) {
+    		case ROUND:
+    	    	intent = new Intent(this, RoundShelfPropertiesActivity.class);
+    	    	startActivity(intent);
+    	    	break;
+    			
+    		case SQUARE:
+    	    	intent = new Intent(this, SquareShelfPropertiesActivity.class);
+    	    	startActivity(intent);
+    	    	break;
+
+    		case OVAL:
+    	    	intent = new Intent(this, OvalShelfPropertiesActivity.class);
+    	    	startActivity(intent);
+    	    	break;
+
+    		case RECTANGULAR:
+    	    	intent = new Intent(this, RectangularShelfPropertiesActivity.class);
+    	    	startActivity(intent);
+    	    	break;
+    	    	
+    	    default:
+    	    	break;
+    	}
 	}
+	
+	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+		
+		/*
+		Object item = parent.getItemAtPosition(pos);
+		if (item.toString() == "rund")
+			shape = Shape.ROUND;
+		if (item.toString() == "oval")
+			shape = Shape.OVAL;
+		if (item.toString() == "quadratisch")
+			shape = Shape.SQUARE;
+		if (item.toString() == "rechteckig")
+			shape = Shape.RECTANGULAR;
+		*/
+		if (pos == 0)
+			shape = Shape.ROUND;
+		if (pos == 1)
+			shape = Shape.OVAL;
+		if (pos == 2)
+			shape = Shape.SQUARE;
+		if (pos == 3)
+			shape = Shape.RECTANGULAR;
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+    	
+    	shape = Shape.NULL;
+    }
 }
--- MobileApp/src/de/htw_berlin/ar/data/Shelf.java
+++ MobileApp/src/de/htw_berlin/ar/data/Shelf.java
@@ -9,9 +9,6 @@ package de.htw_berlin.ar.data;
  */
 public class Shelf {
 	
-	public enum Shape { ROUND, SQUARE, OVAL, RECTANGULAR }
-	
 	public float weight;
 	public float maxBearingLoad;
-	public Shape shape;
 }
